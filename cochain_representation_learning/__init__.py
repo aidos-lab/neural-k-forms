f931659d4bab9a0bab6b674dc561d0997938668d
Bastian Rieck <bastian@rieck.me>
2023-06-02 07:53:40 +0200

    Global data root directory

---
+++ cochain_representation_learning/__init__.py
@@ -0,0 +1,8 @@
+"""General definitions for the package."""
+
+import os.path
+
+# Root directory for data sets. `pytorch-geometric` has the capability
+# to automatically save them in a cache dir. This construction ensures
+# that all data sets are placed in a data folder in the repo root dir.
+DATA_ROOT = os.path.join(os.path.dirname(__file__), "..", "data")
