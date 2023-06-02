b122b83c26e059c1419fd5c0dc9260d70f069cc7
Bastian Rieck <bastian@rieck.me>
2023-06-02 07:37:58 +0200

    Separate module for plotting functions

--- cochain_representation_learning/plotting.py
+++ cochain_representation_learning/plotting.py
@@ -1,15 +1,18 @@
 """Plotting routines for cochains."""
 
-## plot a vector field given a function f: R^2 -> R^2
-def plot_component_vf(f, ax, comp = 0, x_range=5, y_range=5):
-    """ 
-    A function for plotting a component of a vector field given a function f: R^2 -> R^2
+import numpy as np
+
+import torch
+
+
+def plot_component_vf(f, ax, comp=0, x_range=5, y_range=5):
+    """Plot a component of a vector field given by a function f: R^2 -> R^2.
 
     Parameters
     ----------
     f : a Pytorch Sequential object
         A function f: R^2 -> R^2, represented as a Pytorch Sequential object
-    
+
     ax : matplotlib axis object
         The axis on which to plot the vector field
 
@@ -18,7 +21,7 @@ def plot_component_vf(f, ax, comp = 0, x_range=5, y_range=5):
 
     x_range : float
         The range of x values to plot
-    
+
     y_range : float
         The range of y values to plot
 
@@ -26,26 +29,22 @@ def plot_component_vf(f, ax, comp = 0, x_range=5, y_range=5):
     -------
     None
     """
-    x = np.linspace(-x_range,x_range,20)
-    y = np.linspace(-y_range,y_range,20)
-    X,Y = np.meshgrid(x,y)
+    x = np.linspace(-x_range, x_range, 20)
+    y = np.linspace(-y_range, y_range, 20)
+    X, Y = np.meshgrid(x, y)
 
     X = torch.tensor(X).double()
     Y = torch.tensor(Y).double()
 
-
-    U = np.zeros((20,20))
-    V = np.zeros((20,20))
+    U = np.zeros((20, 20))
+    V = np.zeros((20, 20))
     for i in range(20):
         for j in range(20):
-            inp = np.array([X[i,j],Y[i,j]])
+            inp = np.array([X[i, j], Y[i, j]])
             inp = torch.tensor(inp).float()
-            tv = f.forward(inp).reshape(2,c)
+            # TODO (BR): Where is this coming from?
+            tv = f.forward(inp).reshape(2, c)
 
-            U[i,j] = tv[:,comp][0]
-            V[i,j] = tv[:,comp][1]
-    ax.quiver(X,Y,U,V)
-
-
-
-
+            U[i, j] = tv[:, comp][0]
+            V[i, j] = tv[:, comp][1]
+    ax.quiver(X, Y, U, V)
