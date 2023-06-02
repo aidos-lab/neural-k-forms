fb0456180a034a6cb4b166890af8762994872e04
Bastian Rieck <bastian@rieck.me>
2023-06-02 07:45:53 +0200

    Added cochain data matrix generation

--- cochain_representation_learning/chains.py
+++ cochain_representation_learning/chains.py
@@ -73,3 +73,65 @@ def path_length(path):
     for i in range(len(path) - 1):
         length += np.linalg.norm(path[i + 1] - path[i])
     return length
+
+
+def generate_cochain_data_matrix(vf, chain, d=5):
+    """Generate a cochain data matrix from a chain and a vector field.
+
+    Parameters
+    ----------
+    vf : a Pytorch Sequential object
+        The vector field to be applied to the chain
+
+    chain : a torch tensor of shape (r,2,n)
+        The chain to be turned into a cochain data matrix
+
+    d : int
+        The number of steps for the discretization of the chain
+
+    Returns
+    -------
+    out : a torch tensor of shape (r,c)
+        The cochain data matrix
+    """
+
+    # discretize the chain
+    chain = discretize_chain(chain, d)
+
+    # number of simplicies
+    r = chain.shape[0]
+
+    # number of discrete steps
+    d = chain.shape[1]
+
+    # dimension of ambient space
+    n = chain.shape[2]
+
+    # number of feature-cochains in the cochain data matrix
+    c = int(vf[-1].out_features / n)
+
+    # apply the vector field to the discretized chain
+    out = vf(chain).reshape((r, d, n, c))
+
+    # calculate the simplex gradients
+    simplex_grad = chain[:, 1, :] - chain[:, 0, :]
+
+    # swap dimensions n and c in out
+    out = out.permute(0, 1, 3, 2)
+
+    # calculate the inner product of the vector field and the simplex
+    # gradients at each discrete step on each simplex
+    inner_prod = torch.matmul(out, simplex_grad.T / (d - 1))
+
+    # take diagonal of out3 along axis 0 and 3 (this corresponds to
+    # correcting the broadcasted multplication effect)
+    inner_prod = torch.diagonal(inner_prod, dim1=0, dim2=3)
+
+    # permute dimensions 0 and 2 of out4
+    inner_prod = inner_prod.permute(2, 0, 1)
+
+    # apply the trapezoidal rule to the inner product
+    cdm = (inner_prod[:, 1:, :] + inner_prod[:, 0:-1, :]) / 2
+    cdm = cdm.sum(axis=1)
+
+    return cdm
