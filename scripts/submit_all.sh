15a4328d31237ed84ff35248b91882e87737b48a
Bastian Rieck <bastian.rieck@helmholtz-muenchen.de>
2023-06-19 11:37:37 +0200

    Removed `ENZYMES` data set

--- scripts/submit_all.sh
+++ scripts/submit_all.sh
@@ -1,3 +1,3 @@
-for DATASET in AIDS BZR COX2 DHFR ENZYMES PROTEINS; do
-  ./es_lrz.sh $DATASET "python -m cochain_representation_learning.graphs --name ${DATASET} --hidden-dim 128 --max-epochs 100"
+for DATASET in AIDS BZR COX2 DHFR PROTEINS; do
+  ./es_lrz.sh $DATASET "python -m cochain_representation_learning.graphs --name ${DATASET} --hidden-dim 128 --num-steps 10 --max-epochs 100"
 done
