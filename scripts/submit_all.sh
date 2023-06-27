99158e15b7c1512cc92b50cfa0c96a00db92d7aa
Bastian Rieck <bastian.rieck@helmholtz-muenchen.de>
2023-06-27 20:34:54 +0200

    Fixed submission script

--- scripts/submit_all.sh
+++ scripts/submit_all.sh
@@ -1,3 +1,6 @@
-for DATASET in AIDS BZR COX2 DHFR Fingerprint Letter-high Letter-med Letter-low PROTEINS_full; do
-  ./es_lrz.sh $DATASET "python -m cochain_representation_learning.graphs --name ${DATASET} --hidden-dim 128 --num-steps 10 --max-epochs 100"
+for DATASET in AIDS BZR COX2 DHFR ENZYMES Letter-high Letter-med Letter-low PROTEINS_full; do
+  ./es_lrz.sh $DATASET "python -m cochain_representation_learning.graphs --name ${DATASET} --batch-size 16 --hidden-dim 16 --num-steps 5 --max-epochs 100"
+  for BASELINE in GAT GCN GIN; do
+    ./es_lrz.sh $DATASET "python -m cochain_representation_learning.graphs --name ${DATASET} --batch-size 16 --hidden-dim 16 --num-steps 5 --max-epochs 100 --baseline ${BASELINE}"
+  done
 done
