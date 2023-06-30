003137dfe378dade962b44096191cef1d862bf9b
Bastian Rieck <bastian.rieck@helmholtz-muenchen.de>
2023-06-30 13:49:23 +0200

    New submission script

---
+++ scripts/submit_molecule_net.sh
@@ -0,0 +1,6 @@
+for DATASET in BACE BBBP Tox21; do
+  ./es_lrz.sh $DATASET "python -m cochain_representation_learning.graphs --name ${DATASET} --batch-size 32 --hidden-dim 16 --num-steps 5 --max-epochs 100"
+  for BASELINE in GAT GCN GIN; do
+    ./es_lrz.sh $DATASET "python -m cochain_representation_learning.graphs --name ${DATASET} --batch-size 32 --hidden-dim 16 --num-steps 5 --max-epochs 100 --baseline ${BASELINE}"
+  done
+done
