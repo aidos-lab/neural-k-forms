bec02b8b448d684288e264cb5f4909707d7ce840
Bastian Rieck <bastian.rieck@helmholtz-muenchen.de>
2023-08-30 17:49:23 +0200

    Updated `MoleculeNet` submission script

--- scripts/submit_molecule_net.sh
+++ scripts/submit_molecule_net.sh
@@ -1,6 +1,11 @@
-for DATASET in BACE BBBP; do
-  ./es_lrz.sh $DATASET "python -m cochain_representation_learning.graphs --name ${DATASET} --batch-size 32 --hidden-dim 64 --num-steps 10 --max-epochs 100"
-  for BASELINE in GAT GCN GIN; do
-    ./es_lrz.sh $DATASET "python -m cochain_representation_learning.graphs --name ${DATASET} --batch-size 32 --hidden-dim 64 --num-steps 10 --max-epochs 100 --baseline ${BASELINE}"
+# Submit `MoleculeNet` data sets. In contrast to the small TU data sets,
+# we just repeat the training procedure five times.
+
+for i in `seq 5`; do
+  for DATASET in BACE BBBP; do
+    ./es_lrz.sh $DATASET "python -m cochain_representation_learning.graphs --name ${DATASET} --batch-size 32 --hidden-dim 64 --num-steps 10 --max-epochs 100"
+    for BASELINE in GAT GCN GIN; do
+      ./es_lrz.sh $DATASET "python -m cochain_representation_learning.graphs --name ${DATASET} --batch-size 32 --hidden-dim 64 --num-steps 10 --max-epochs 100 --baseline ${BASELINE}"
+    done
   done
 done
