dbbbda8df5aaaf78540cc1f161e66792b2549f8c
Bastian Rieck <bastian.rieck@helmholtz-muenchen.de>
2023-08-30 17:53:26 +0200

    Fixed baseline call

--- scripts/submit_molecule_net.sh
+++ scripts/submit_molecule_net.sh
@@ -5,7 +5,7 @@ for i in `seq 5`; do
   for DATASET in BACE BBBP; do
     ./es_lrz.sh $DATASET "python -m cochain_representation_learning.graphs --name ${DATASET} --batch-size 32 --hidden-dim 64 --num-steps 10 --max-epochs 100 --tag final"
     for BASELINE in GAT GCN GIN; do
-      ./es_lrz.sh $DATASET "python -m cochain_representation_learning.graphs --name ${DATASET} --batch-size 32 --hidden-dim 64 --num-steps 10 --max-epochs 100 -tag final --baseline ${BASELINE}"
+      ./es_lrz.sh $DATASET "python -m cochain_representation_learning.graphs --name ${DATASET} --batch-size 32 --hidden-dim 64 --num-steps 10 --max-epochs 100 --tag final --baseline ${BASELINE}"
     done
   done
 done
