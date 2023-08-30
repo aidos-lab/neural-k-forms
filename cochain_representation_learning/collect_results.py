cac7eaf32e84404f4727345c89f47058208eb1d4
Bastian Rieck <bastian@rieck.me>
2023-08-30 13:48:58 +0200

    Basic grouping for preparing results

---
+++ cochain_representation_learning/collect_results.py
@@ -0,0 +1,22 @@
+"""Prepare results based on `wandb.ai` output files."""
+
+import argparse
+import pandas as pd
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("INPUT", type=str, help="Input file")
+
+    args = parser.parse_args()
+
+    df = pd.read_csv(args.INPUT)
+
+    columns = ["dataset", "model", "test_accuracy"]
+    columns_to_drop = [col for col in df.columns if col not in columns]
+
+    df = df.drop(columns_to_drop, axis="columns")
+    df["test_accuracy"] *= 100.0
+
+    grouped = df.groupby(["dataset", "model"]).agg(["mean", "std"])
+    print(grouped)
