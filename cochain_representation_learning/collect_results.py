5e800fe08417c0bb36fbb5d83d67f25b0c93ef8d
Bastian Rieck <bastian@rieck.me>
2023-09-01 15:08:09 +0200

    Made column selectable for aggregation script

--- cochain_representation_learning/collect_results.py
+++ cochain_representation_learning/collect_results.py
@@ -7,15 +7,16 @@ import pandas as pd
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("INPUT", type=str, help="Input file")
+    parser.add_argument("-c", "--column", type=str, default="test_accuracy")
 
     args = parser.parse_args()
 
     df = pd.read_csv(args.INPUT)
 
-    columns = ["dataset", "model", "test_accuracy"]
+    columns = ["dataset", "model", args.column]
     columns_to_drop = [col for col in df.columns if col not in columns]
 
     df = df.drop(columns_to_drop, axis="columns")
-    df["test_accuracy"] *= 100.0
+    df[args.column] *= 100.0
 
     print(df.groupby(["dataset", "model"]).agg(["mean", "std"]).round(2))
