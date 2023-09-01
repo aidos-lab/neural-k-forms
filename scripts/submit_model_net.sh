3298c588839999f23d00462c3cc8f3ac22917328
Bastian Rieck <bastian.rieck@helmholtz-muenchen.de>
2023-09-01 09:41:49 +0200

    Added `ModelNet` submission script

---
+++ scripts/submit_model_net.sh
@@ -0,0 +1,11 @@
+# Submit `ModelNet` data sets. In contrast to the small TU data sets, we
+# just repeat the training procedure five times.
+
+for i in `seq 5`; do
+  for DATASET in ModelNet10 ModelNet40; do
+    ./es_lrz.sh $DATASET "python -m cochain_representation_learning.graphs --name ${DATASET} --batch-size 32 --hidden-dim 64 --num-steps 10 --max-epochs 100 --tag final"
+    for BASELINE in GAT GCN GIN; do
+      ./es_lrz.sh $DATASET "python -m cochain_representation_learning.graphs --name ${DATASET} --batch-size 32 --hidden-dim 64 --num-steps 10 --max-epochs 100 --tag final --baseline ${BASELINE}"
+    done
+  done
+done
