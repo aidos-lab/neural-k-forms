06245af3b440f14abefe70452af62f5a474fdd81
celiahacker <hackercelia@gmail.com>
2023-10-24 15:15:55 +0100

    finished cleaning 2-form code

--- neural_kforms.py
+++ neural_kforms.py
@@ -1,5 +1,5 @@
 import numpy as np
-import matplotlib as mpl
+import matplotlib as mpl  
 import torch  
 import torch.nn as nn
 from matplotlib import pyplot as plt
@@ -386,21 +386,28 @@ def build_boundaries(simplices):
 # -----------------------------------------------------------
 
 
-# subdivide unit square and return a single array of points
+# ---------
+# Data generation functions
+# ---------
+
 def unit_square_grid(n=3):
+    """
+    Make a grid of points in the unit square [0,1] x [0,1]
+
+    Parameters:
+    n : int
+        The number of points in each direction
+    """
     x = np.linspace(0, 1, n)
     y = np.linspace(0, 1, n)
     X, Y = np.meshgrid(x, y)
     return np.array([X.flatten(), Y.flatten()]).T
 
-## return the alpha complex of a unit square grid
-def unit_square_grid_complex(n=3):
-    pts = unit_square_grid(n)
-    ac = gd.AlphaComplex(pts).create_simplex_tree()
-    return ac
-
-# plot and return the alpha complex of a unit square grid
 def plot_unit_square_grid(n=3):
+    """ 
+    Plot the alpha complex of a grid in the unit square
+    Return: the points and the alpha complex
+    """
     pts = unit_square_grid(n)
     ac = gd.AlphaComplex(pts).create_simplex_tree()
     plt.scatter(pts[:,0], pts[:,1])
@@ -412,25 +419,54 @@ def plot_unit_square_grid(n=3):
 
 
 def random_surface_sin_x(n, scale = 1, period = 1, eps = 0.1):
-    trans_x = np.random.uniform(-10, 10, 1)
-    trans_y = np.random.uniform(-10, 10, 1)
-    x = np.arange(0, n, 1)/scale + trans_x
-    y = np.arange(0, n, 1)/scale + trans_y
-    Eps = np.random.uniform(-eps,eps, (n,n))    
-    X, Y = np.meshgrid(x, y)
-    Z = np.zeros(X.shape)
+    """ 
+    Generate a random surface in R^3 with a sine wave in the x direction
+
+    Input: 
+        n: number of points in each direction
+        scale: scale of the surface
+        period: period of the sine wave
+        eps: magnitude of the noise
+    Output:
+        X, Y, Z: the coordinates of the surface
+        ar: an array of points    
+    """
+
+    trans_x = np.random.uniform(-10, 10, 1) # translation in x direction
+    trans_y = np.random.uniform(-10, 10, 1) # translation in y direction 
+
+    x = np.arange(0, n, 1)/scale + trans_x # x coordinates
+    y = np.arange(0, n, 1)/scale + trans_y # y coordinates
+
+    Eps = np.random.uniform(-eps,eps, (n,n)) # noise  
+
+    X, Y = np.meshgrid(x, y) # create a grid of points
+    Z = np.zeros(X.shape) # initialize the z coordinates
 
     for i in range(X.shape[0]):
         for j in range(X.shape[1]):
             Z[i,j] = math.sin(X[i,j]/period) 
-            # return an array of points
-    Z = Z + Eps     
+    
+    Z = Z + Eps # add noise to the surface 
     ar = np.array([X.flatten(), Y.flatten(), Z.flatten()]).T
     ar = torch.from_numpy(ar)
 
     return X, Y, Z, ar
 
 def random_surface_sin_y(n, scale =1, period =1, eps = 0.1):
+    """
+    Generate a random surface in R^3 with a sine wave in the y direction
+
+    Input:
+        n: number of points in each direction
+        scale: scale of the surface
+        period: period of the sine wave
+        eps: magnitude of the noise
+    Output:
+        X, Y, Z: the coordinates of the surface
+        ar: an array of points
+    """
+
     trans_x = np.random.uniform(-10, 10, 1)
     trans_y = np.random.uniform(-10, 10, 1)
     x = np.arange(0, n, 1)/scale + trans_x
@@ -451,21 +487,55 @@ def random_surface_sin_y(n, scale =1, period =1, eps = 0.1):
 
 
 def generate_surfaces_sin_x(num_surf, n_pts, scale =1, period =1, eps = 0.1): 
+    """
+    Generate a list of random surfaces in R^3 with a sine wave in the x direction
+
+    Input:
+        num_surf: number of surfaces to generate
+        n_pts: number of points in each direction
+        scale: scale of the surface
+        period: period of the sine wave
+        eps: magnitude of the noise
+    Output:
+        surfaces: a list of num_surf surfaces 
+    """
     surfaces = []
     for i in range(num_surf):
         surfaces.append(random_surface_sin_x(n_pts, scale = scale, period = period, eps = eps))
     return surfaces
 
 def generate_surfaces_sin_y(num_surf, n_pts, scale =1, period =1, eps = 0.1): 
+    """
+    Generate a list of random surfaces in R^3 with a sine wave in the y direction
+    
+    Input:
+        num_surf: number of surfaces to generate
+        n_pts: number of points in each direction
+        scale: scale of the surface
+        period: period of the sine wave
+        eps: magnitude of the noise
+    Output:
+        surfaces: a list of num_surf surfaces 
+    """
+
     surfaces = []
     for i in range(num_surf):
         surfaces.append(random_surface_sin_y(n_pts, scale = scale, period = period, eps = eps))
     return surfaces
 
 
+#-------- 
+# Functions to help integration of 2-forms
+#--------
+
 
 def subdivide_simplex_torch(n):
-    # create a list of vertices
+    """
+    Subdivide a 2-simplex into n^2 2-simplices
+    ---- 
+    Input: n the number of subdivision of the simplex
+    Output: a list of vertices of the subdivision of the simplex
+    """
     vertices = []
     for i in range(n+1):
         for j in range(n+1-i):
@@ -473,16 +543,20 @@ def subdivide_simplex_torch(n):
     vertices = torch.tensor(vertices)
     return vertices
 
-# check if a point is strictly the interior of a triangle with vertices (0,0), (1,0), (0,1)
 def is_interior(point):
+    """ 
+    check if a point is strictly in the interior of a triangle with vertices (0,0), (1,0), (0,1)
+    """
     if point[0] > 0 and point[1] > 0 and point[0] + point[1] < 1:
         return True
     else:
         return False
-    
 
-# check if a point is striclty on the intetior one of the edges of a triangle with vertices (0,0), (1,0), (0,1)
 def is_edge(point):
+    """
+    check if a point is on the edge of a triangle with vertices (0,0), (1,0), (0,1)
+    """
+
     if point[0] == 0 and point[1] > 0 and point[1] < 1:
         # vertical edges
         return True
@@ -495,8 +569,10 @@ def is_edge(point):
     else:
         return False
 
-#check if a point is a vertex of a triangle with vertices (0,0), (1,0), (0,1)
 def is_vertex(point):
+    """
+    check if a point is a vertex of a triangle with vertices (0,0), (1,0), (0,1)
+    """
     if point[0] == 0 and point[1] == 0:
         return True
     elif point[0] == 1 and point[1] == 0:
@@ -507,18 +583,26 @@ def is_vertex(point):
         return False
     
 def coef_vertex(v): 
+    """
+    compute the contrbution of a vertex of the subdivision in the integration formula
+
+    If the vertex is in the interior, the vertex is part of 6 2-simplices 
+    If the vertex is on an edge, the vertex is part of 3 2-simplices
+    If the vertex is a vertex, the vertex is part of 1 2-simplex
+    """
+
     if is_interior(v):
         return torch.tensor([6]).float()
-        #return 6
     if is_edge(v):
         return torch.tensor([3]).float()
-        #return 3
     if is_vertex(v):
         return torch.tensor([1]).float()
-        #return 1
 
 def subdivide_simplex_coef_torch(n):
-    # create a list of vertices
+    """
+    Input: n the number of subdivision of the simplex
+    Output: a list of vertices of the subdivision of the simplex and the coefficients for the contribution of each vertex
+    """
     vertices = []
     for i in range(n+1):
         for j in range(n+1-i):
@@ -532,15 +616,14 @@ def subdivide_simplex_coef_torch(n):
     return vertices, coefs
 
 
-
 def phi_b(embedded_vertices):
     """build the affine transforamtion matrix that corresponds to the embedded vertices"""
-    dim = embedded_vertices.shape[1] ### check this
-    #print(dim)
+    #dim = embedded_vertices.shape[1] 
+    
     a = [embedded_vertices[i] - embedded_vertices[0] for i in range(1,3)]
     phi = torch.stack(a)
     b = embedded_vertices[0] 
-    ## should we return the transpose of phi and b?
+    
     return phi.float(), b.float()
 
 def build_determinant_tensor(dim, k =2):
@@ -549,8 +632,7 @@ def build_determinant_tensor(dim, k =2):
         Output: Tensors of size (n choose k) x n x n that can be used to compute the determinant entering in the integration formuala
         of  k-form over an embedded simplex
     """
-    ### Can this be writen in a more efficient way? i.e. without using a for loop
-    
+
     N = int(scipy.special.binom(dim,k))
     deter_tensor = torch.zeros(N, dim, dim)
     ind = 0
@@ -564,9 +646,13 @@ def build_determinant_tensor(dim, k =2):
     return deter_tensor.float()
 
 
+# ----- 
+# Functions for integrating 2-forms and creating cochains 
+# -----
+
 def integrate_kform(kform, phi, b, det, subdivision_vert, subdivision_coefs, num_sub, dim = 3, k = 2):
                     
-    """ Integrate a $2$-form over $2$-simplex
+    """ Integrate a single 2-form over 2-simplex
     Input: 
         kform: a 2-form
         phi: a matrix containing the embedding of the vertices of the simplex in R^dim
@@ -580,9 +666,6 @@ def integrate_kform(kform, phi, b, det, subdivision_vert, subdivision_coefs, num
     Output:
         integral: the value of integral of the 2-form over the 2-simplex 
     """
-
-    ## TODO: add assert to check inputs are good 
-
    
     num_simplices = int(num_sub**2)
     integral = torch.tensor([0]).float()
@@ -604,18 +687,19 @@ def integrate_kform(kform, phi, b, det, subdivision_vert, subdivision_coefs, num
 
 def form2cochain(kform, surface_dict, deter_tensor,subivision_vert, subdivision_coeffs, num_sub, dim =3 ,k = 2): 
 
-    """From a 2-form we get a 2-cochain by integrating the form over every 2-simplex in the simplicial complex
+    """From a 2-form we get a cochain by integrating the form over every 2-simplex in the simplicial complex
+
     Input:
         kform: a 2-form
         surface_dict: a dictionary containing all of the information of the triangulated surface in R^dim
-        deter_tensor: the tensor computed by build_determinant_tensor
+        deter_tensor: the tensor computed by build_determinant_tensor 
         dim: the dimension of the embedding space
         k: the dimension of the form
         subivision_vert: a list of vertices of the subdivision of the simplex
         subdivision_coefs: a list of coefficients for the contribution of each vertex to the integral
     
     Output:
-        cochain: a 2-cochain, torch tensor of shape FILL IN!!!!
+        cochain: a cochain on the simplicial complex  
      """
 
     Emb_comp = surface_dict['points']
@@ -630,10 +714,11 @@ def form2cochain(kform, surface_dict, deter_tensor,subivision_vert, subdivision_
 
     return cochain
 
-#### multiple cocycle functions
+
 
 def integrate_kforms(kform, phi, b, det, subdivision_vert, subdivision_coefs, num_sub, l =1, dim = 3, k = 2):
-    """ Integrate l 2-forms over 2-simplex
+    """ Integrate multiple 2-forms over a 2-simplex
+    --------
     Input: 
         kform: l 2-forms
         phi: a matrix containing the embedding of the vertices of the simplex in R^dim
@@ -648,9 +733,6 @@ def integrate_kforms(kform, phi, b, det, subdivision_vert, subdivision_coefs, nu
     Output:
         integral: the value of integral of the l 2-forms over the 2-simplex 
     """
-
-    ## TODO: add assert to check inputs 
-    ## check  that l and the shape of the k-form are consistent
 
     ## number of vertices in the subdivision
     num_simplices = int(num_sub**2) ## number of simplices in the subdivision
@@ -683,6 +765,24 @@ def integrate_kforms(kform, phi, b, det, subdivision_vert, subdivision_coefs, nu
     return   ((vol/num_simplices)*integrals).float()
 
 def forms2cochains(kform,surface_dict, deter_tensor, subdivision_vert, subdivision_coef, num_sub, l = 1, dim = 3, k = 2):
+    """ 
+    From l 2-forms we get l cochains on the complex by integrating the forms over every 2-simplex in the simplicial complex
+
+    Input:
+        kform: l 2-forms
+        surface_dict: a dictionary containing all of the information of the triangulated surface in R^dim
+        deter_tensor: the tensor computed by build_determinant_tensor 
+        dim: the dimension of the embedding space
+        k: the dimension of the form
+        subivision_vert: a list of vertices of the subdivision of the simplex
+        subdivision_coefs: a list of coefficients for the contribution of each vertex to the integral
+        num_sub: the number of subdivisions of the simplex for the approximation of the integral
+    
+    Output:
+        cochains: l cochains on the simplicial complex
+    """
+
+
     Emb_comp = surface_dict['points']
 
     assert Emb_comp.shape[1] == dim, "The dimension of the embedding space is not equal to the number of columns of the matrix Emb_comp"
