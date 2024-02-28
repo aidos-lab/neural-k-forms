545936f5be121a69e7a71dddcccb4ebfe02766bd
Bastian Rieck <bastian@rieck.me>
2024-02-28 10:19:29 +0100

    Removed obsolete code from chains module

--- neural_k_forms/chains.py
+++ neural_k_forms/chains.py
@@ -5,33 +5,6 @@ import numpy as np
 import torch
 
 
-def path_to_chain(p):
-    """Turn a path into a chain.
-
-    Parameters
-    ----------
-    p : numpy array
-        A path in R^n, represented as a numpy array of shape (p,n),
-        where p is the number of points in the path
-
-    Returns
-    -------
-    chain : numpy array
-        A chain in R^n, represented as a numpy array of shape (p-1,2,n),
-        where p is the number of points in the path. The middle index
-        corresponds to start and endpoints of the edges in the chain.
-    """
-    r = len(p) - 1
-    n = p[0].shape[0]
-
-    chain = torch.zeros((r, 2, n))
-
-    chain[:, 1, :] = torch.tensor(p[1:, :])
-    chain[:, 0, :] = torch.tensor(p[0:-1, :])
-
-    return chain
-
-
 def discretize_chain(chain, d):
     """Discretize a chain.
 
@@ -63,16 +36,6 @@ def discretize_chain(chain, d):
         d_chain[:, i, :] = (1 - t[i]) * chain[:, 0, :] + t[i] * chain[:, 1, :]
 
     return d_chain
-
-
-# TODO (BR): Is this still required? There are some assumptions here
-# about the norm that we could maybe turn into parameters.
-def path_length(path):
-    """Calculate the length of a path."""
-    length = 0
-    for i in range(len(path) - 1):
-        length += np.linalg.norm(path[i + 1] - path[i])
-    return length
 
 
 def generate_cochain_data_matrix(vf, chain, d=5):
